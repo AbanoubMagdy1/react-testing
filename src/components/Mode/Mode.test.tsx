94832d8560ef5f1ee6888453362673fc88cbe35e
AbanoubMagdy1 <bebomagdy666@gmail.com>
2023-01-03 12:12:12 +0200

    add mode commect

--- src/components/Mode/Mode.test.tsx
+++ src/components/Mode/Mode.test.tsx
@@ -4,6 +4,9 @@ import Mode from "./Mode";
 import AppProvider from "../../providers/AppProvider";
 import userEvent from '@testing-library/user-event'
 
+//We can user custom render function to wrap our component with AppProvider
+//See https://testing-library.com/docs/react-testing-library/setup/#custom-render
+
 test("Mode component", async () => {
     render(<Mode />, { wrapper: AppProvider });
     const user = userEvent.setup()
